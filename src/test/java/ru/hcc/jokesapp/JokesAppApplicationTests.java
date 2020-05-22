76b5b3870403ed8bd5f5784a2df85fa0757e57c5
Rewazer <runner.px@gmail.com>
2020-05-22 17:44:37 +0500

    Add Project

---
+++ src/test/java/ru/hcc/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hcc.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
